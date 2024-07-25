c86b0e2640de857574e103c3ac0366f4c07765f9
Vyacheslav <sbhlrsa@gmail.com>
2024-07-25 17:20:01 +0400

    add this year

---
+++ my-task-1/src/App.js
@@ -0,0 +1,28 @@
+// Весь код ниже в декларативном стиле, императивного стиля я не вижу
+
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+				<p>This year {new Date().getFullYear()}</p>
+			</header>
+		</div>
+	);
+}
+
+export default App;
