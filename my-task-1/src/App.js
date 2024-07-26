2d39b01a9be21510785b583b72cf87d182956ea5
Vyacheslav <sbhlrsa@gmail.com>
2024-07-26 13:39:03 +0400

    Переписал код без ищспользования JSX

--- my-task-1/src/App.js
+++ my-task-1/src/App.js
@@ -2,27 +2,53 @@
 
 import logo from "./logo.svg";
 import "./App.css";
+import { createElement } from "react";
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>This year {new Date().getFullYear()}</p>
-			</header>
-		</div>
+	return createElement(
+		"div",
+		{ className: "App" },
+		createElement(
+			"header",
+			{ className: "App-header" },
+			createElement("img", {
+				src: logo,
+				className: "App-logo",
+				alt: "logo",
+			}),
+			createElement("p", null, "Edit src/App.js and save to reload."),
+			createElement(
+				"a",
+				{
+					className: "App-link",
+					href: "https://reactjs.org",
+					target: "_blank",
+					rel: "noopener noreferrer",
+				},
+				"Learn React"
+			),
+			createElement("p", null, `This year ${new Date().getFullYear()}`)
+		)
 	);
+	// return (
+	// 	<div className="App">
+	// 		<header className="App-header">
+	// 			<img src={logo} className="App-logo" alt="logo" />
+	// 			<p>
+	// 				Edit <code>src/App.js</code> and save to reload.
+	// 			</p>
+	// 			<a
+	// 				className="App-link"
+	// 				href="https://reactjs.org"
+	// 				target="_blank"
+	// 				rel="noopener noreferrer"
+	// 			>
+	// 				Learn React
+	// 			</a>
+	// 			<p>This year {new Date().getFullYear()}</p>
+	// 		</header>
+	// 	</div>
+	// );
 }
 
 export default App;
